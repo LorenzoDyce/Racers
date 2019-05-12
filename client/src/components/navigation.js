1b3b957e586924d6a81412e0ae1c35c8c196e36b
Lorenzo <lpdmanagement@gmail.com>
2019-05-11 21:34:11 -0400

    navigation component initial setup

---
+++ client/src/components/navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import '../navigation.css';
+
+
+const Navigation = () => {
+    return ( 
+        <div className="links">
+         <NavLink to='/' activeClassName="current">Home</NavLink>
+         <NavLink to='/createdrivers' activeClassName="current">Sign Up</NavLink>  
+         <NavLink to='/driverscars' activeClassName="current">Drivers</NavLink>      
+        </div>
+     );
+}
+ 
+export default Navigation;
