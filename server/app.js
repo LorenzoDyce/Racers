22331a78e86882f4316b96132e63a08ba5f7ca16
Lorenzo <lpdmanagement@gmail.com>
2019-05-11 21:36:10 -0400

    cors middleware setup

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 
 
@@ -10,10 +11,20 @@ const routes = require('./routes/index');
 const drivers  = require('./routes/drivers');
 
 const app = express();
-
-
+app.use(cors({origin: 'http://localhost:3000',
+credentials: true}))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.json())
+app.use((req, res, next) => {
+          res.header("Access-Control-Allow-Origin", "*");
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+          if(req.method === 'OPTIONS') {
+              res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE', 'GET')
+              return res.status(200).json({});
+          }
+          next();
+}); 
 
 
 app.use('/', routes);
