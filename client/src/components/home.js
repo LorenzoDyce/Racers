5b45cb1ab2de7fd953e71c39e6e75122d9b5de52
LorenzoDyce <lpdmanagement@gmail.com>
2019-05-11 21:35:44 -0400

    initial home component setup

---
+++ client/src/components/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+
+
+
+const Home = () =>  {
+        return ( 
+            <div>
+                <h1>WELCOME TO CARBOOK</h1>
+            </div>
+         );
+    }
+ 
+export default Home;
