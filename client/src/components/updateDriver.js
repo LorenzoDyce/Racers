9b6e967d55a4ae7d35c298897d076f531beb7dc5
Lorenzo <lpdmanagement@gmail.com>
2019-05-14 18:55:18 -0400

    Initial Update Driver component

---
+++ client/src/components/updateDriver.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+
+class UpdateDriver extends Component {
+    state = { 
+     name: '',
+     nationality: '',
+     bio: ''
+     }
+
+    handleChange = (event) => {
+    this.setState({[event.target.name]: event.target.value})
+    }
+
+   handleSubmit = (event) => {
+   event.preventDefault();
+   const {name, nationality, bio } = this.state;
+   const driver = {
+       name,
+       nationality,
+       bio
+   }
+   axios.put(`/updatedriver/:driverId`, driver)
+   .then(res => console.log(res.data));
+   }
+
+
+
+
+    render() { 
+        const  {name, nationality, bio} =this.state;
+        return (  
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" name="name" value={name} onChange={this.handleChange} />
+                </label>
+                <label>
+                    Nationality:
+                    <input type="text" name="nationality" value={nationality} onChange={this.handleChange}/>
+                </label> 
+
+                <label>
+                    Bio:
+                    <input type="text" name="bio" value={bio} onChange={this.handleChange}/>
+                </label>
+                <button type="submit">Update</button>
+                </form>
+            </div>
+        );
+    }
+}
+ 
+export default UpdateDriver;
