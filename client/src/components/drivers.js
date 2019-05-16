89821344fd74aa9a1197b979ac742f0b847b0e92
LorenzoDyce <lpdmanagement@gmail.com>
2019-05-16 19:44:27 -0400

    imported the handUpdate and handleDelete feature.

--- client/src/components/drivers.js
+++ client/src/components/drivers.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import axios from 'axios'
-import '../Drivers.css'
+import axios from 'axios';
+import '../Drivers.css';
+
 
 
 
@@ -24,13 +25,50 @@ class Drivers extends Component {
          this.setState({drivers});
         })
     }
+
+
+    handleUpdate = (event) => {
+        event.preventDefault();
+        const {name, nationality, bio } = this.state;
+        const driver = {
+            name,
+            nationality,
+            bio
+        }
+        axios.put(`/updatedriver/${this.state.id}`, driver)
+        .then(res => console.log(res.data));
+        }
+
+
+
+        handleDelete = event => {
+            event.preventDefault();
+            const {id, name, nationality, bio } = this.state;
+            const driver = {
+                id,
+                name,
+                nationality,
+                bio
+            };
+   
+       axios.delete(`/deletedriver/${this.state.id}`, driver.id)
+          .then(res => {
+           console.log(res);
+           console.log(res.data);
+       })
+       .catch(error => {
+           console.log(error);
+       })
+   }
+
     render() { 
         return ( 
         <div>
             <h1>Drivers</h1>
             <ul className='driverinfo'>
             {this.state.drivers.map(driver => 
-            <li key={driver.id}>{driver.name} {driver.nationality} {driver.bio}</li>
+            <li key={driver.id}>{driver.name} {driver.nationality} {driver.bio}  <button onClick={this.handleUpdate}>Edit</button>
+            <button onClick={this.handleDelete}>Delete</button> </li>
             )}
             </ul>
         </div> 
